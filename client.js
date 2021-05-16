2fd5fc93642db640e6b6209e863411dd0d6fe393
Jerry Xiong <xion0597@umn.edu>
2021-05-16 00:30:49 -0500

    added some more comments, made the monthly total round to the nearest cent

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 console.log('JS loaded');
 
-let employees = [] // create array holding box to be pushed my objects
-
+// Variable Initialization
+let employees = [] // create array holding box to be pushed by objects
 
 // let employee = {} // create empty object to put values into and push into array
 
@@ -13,31 +13,25 @@ $(document).ready(readyNow)
 function readyNow(){
     console.log('JQuery loaded');
     
-// Step 1: Make a Function that takes in all inputs and adds them into an object than returns an array
 
                 //Click Listeners
+
+// Step 1: Make a Function that takes in all inputs and adds them into an object than returns an array
 $('#submitBtn').on('click', addEmployee)
 
 
 // Step 2: Make a function that takes the input into a new row in the table on click
-
 $('#submitBtn').on('click', newRow)
-
 $('#submitBtn').on('click', clearInput) // resets input
 
 
 // Step 3: Add a function to the delete button to empty the current row
-
 $('#output').on('click', '.deleteBtn', deleteRow)
 
 
 // Step 4: Implement a function to calculate the monthly total of each employee
-
-$('#submitBtn').on('click', monthlyCosts)
-
-
 // Step 5: manipulate the function to highlight monthly cost if > 20,000
-
+$('#submitBtn').on('click', monthlyCosts)
 
 
 console.log(employees[0]); // testing for objects in array
@@ -103,14 +97,14 @@ function monthlyCosts(){
     let totalSalary = 0;
     let monthly = 0;
     for (i = 0; i < employees.length; i++) {
-       totalSalary += parseInt(employees[i].annualSalary)
+       totalSalary += Number(employees[i].annualSalary)
     }
     monthly = totalSalary / 12;
     
-    $('#monthlyTotal').text(`Monthly Total: $${monthly}`) // updates DOM of monthly prices
+    $('#monthlyTotal').text(`Monthly Total: $${monthly.toFixed(2)}`) // updates DOM of monthly prices
 
     if (monthly > 20000) {
         $('#monthlyTotal').addClass('highlight')
-    }
+    } // adds red background
     return console.log(monthly);
-}
+} // end monthlyCosts
