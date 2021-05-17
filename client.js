75aae4b24b5369aa5c5c1225efbc90968ae896f0
Jerry Xiong <xion0597@umn.edu>
2021-05-17 11:38:11 -0500

    added stringtolocale

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ console.log('JS loaded');
 // Variable Initialization
 let employees = [] // create array holding box to be pushed by objects
 
+
 // let employee = {} // create empty object to put values into and push into array
 
 $(document).ready(readyNow)
@@ -34,7 +35,7 @@ $('#output').on('click', '.deleteBtn', deleteRow)
 $('#submitBtn').on('click', monthlyCosts)
 
 
-console.log(employees[0]); // testing for objects in array
+console.log('employees', employees); // testing for objects in array
 
 } // end readyNow
 
@@ -44,51 +45,89 @@ console.log(employees[0]); // testing for objects in array
 function addEmployee(){
     console.log('button clicked');
     
-    const employee = {}
+    // const employee = {}
     
-    employee.firstName = $('#firstName').val();
-    employee.lastName = $('#lastName').val();
-    employee.iD = $('#iD').val();
-    employee.title = $('#title').val();
-    employee.annualSalary = $('#annualSalary').val();
+    // employee.firstName = $('#firstName').val();
+    // employee.lastName = $('#lastName').val();
+    // employee.iD = $('#iD').val();
+    // employee.title = $('#title').val();
+    // employee.annualSalary = $('#annualSalary').val();
     
-    employees.push(employee)
+    // employees.push(employee)
 
-    return console.log(employees);
-    ;
+            // OPTIMAL OBJECT CREATION
+        let employee = {
+            firstName: $('#firstName').val(),
+            lastName: $('#lastName').val(),
+            iD: $('#iD').val(),
+            title: $('#title').val(),
+            annualSalary: $('#annualSalary').val()
+        }
+        employees.push(employee)
 } // end addEmployee
 
 
 // function to clear inputs
 function clearInput(){
-    $('#firstName').val('')
-    $('#lastName').val('')
-    $('#iD').val('')
-    $('#title').val('')
-    $('#annualSalary').val('')
+    // $('#firstName').val('')
+    // $('#lastName').val('')
+    // $('#iD').val('')
+    // $('#title').val('')
+    // $('#annualSalary').val('')
+
+            // OPTIMAL SOLUTION --> make all inputs a class and make clear it
+            $('.inputs').val('')
 } // end clearInput
 
 
-function newRow(){
+function newRow(){  // --> should be named renderEmployee
+
     $('#output').append(`
     <tr>
-    <td class="info">${$('#firstName').val()}</td>
-    <td class="info">${$('#lastName').val()}</td>
-    <td class="info">${$('#iD').val()}</td>
-    <td class="info">${$('#title').val()}</td>
+    <td>${$('#firstName').val()}</td>
+    <td>${$('#lastName').val()}</td>
+    <td>${$('#iD').val()}</td>
+    <td>${$('#title').val()}</td>
     <td class="info">$${$('#annualSalary').val()}</td>
-    <td class="info">
+    <td>
     <button class="deleteBtn btn btn-secondary btn-sm"">Delete</button>
     </td>
     </tr>
-    `)   
+    `)
+
+            // OPTIMAL TABLE CREATION
+// $('#output').empty(). // clears old data on the DOM
+
+// for (let employee of employees){
+//     $('#output').append(`
+//     <tr>
+//         <td>${employee.firstName})</td>
+//         <td>${employee.lastName}</td>
+//         <td>${employees.iD}</td>
+//         <td>${employee.title}</td>
+//         <td class="info">$${employee.annualSalary}</td>
+//         <td>
+//             <button class="deleteBtn btn btn-secondary btn-sm"">Delete</button>
+//         </td>
+//     </tr>`)                   // pushes new data onto DOM
+
+//     }// end loop
 } // end newRow
 
 
-// function for delete button
+// function for delete button  --> Stretch Goal: Calculate the removal of an Employee's Monthly Cost
 function deleteRow(){
     console.log('deleteBtn clicked');
-    $(this).parent().parent().empty();
+   
+// target specific employee annual salary
+
+    console.log($('.info').text());
+
+// Call monthlyCosts function to update costs
+
+
+
+$(this).closest('tr').empty();
 } // end deleteRow
 
 
@@ -99,12 +138,12 @@ function monthlyCosts(){
     for (i = 0; i < employees.length; i++) {
        totalSalary += Number(employees[i].annualSalary)
     }
-    monthly = totalSalary / 12;
+    monthly = (totalSalary / 12)
     
-    $('#monthlyTotal').text(`Monthly Total: $${monthly.toFixed(2)}`) // updates DOM of monthly prices
+    $('#monthlyTotal').text(`Monthly Total: ${monthly.toLocaleString('en-EN', {style: 'currency', currency: 'USD'})}`) // updates DOM of monthly prices
 
     if (monthly > 20000) {
         $('#monthlyTotal').addClass('highlight')
     } // adds red background
-    return console.log(monthly);
+    return console.log('Total monthly costs:$', monthly);
 } // end monthlyCosts
